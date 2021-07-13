ec2da1273cd69e6b3df71ece977f35d52aec4516
Nelson Shilman <NMShilman@teco.com.ar>
2021-07-13 14:00:27 -0300

    add app to deploy

---
+++ gantt/app.R
@@ -0,0 +1,29 @@
+source('script.R')
+
+library(shiny)
+
+# Define UI for application 
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Gantt curso DS"),
+
+
+        # Show a plot of the gantt
+        mainPanel(
+            plotlyOutput("gantt")
+        )
+    )
+# )
+
+# Define server logic required 
+server <- function(input, output) {
+
+    output$gantt <- renderPlotly({
+        
+p
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ gantt/script.R
@@ -0,0 +1,34 @@
+library(plotly)
+library(tidyverse)
+library(lubridate)
+
+gantt_df <- data.frame(
+  modules = c(paste0('Módulo ', 1:8), 'Revisión y edición'),
+  start_date = c(rep(dmy(160721),2), rep(dmy(190721),2),  rep(dmy(260721),2),  rep(dmy(020821),2), dmy(090821)),
+  end_date = c(rep(dmy(190721),2), rep(dmy(260721),2),  rep(dmy(020821),2),  rep(dmy(090821),2), dmy(160821)),
+  completion = rep('0%', 9),
+  color = c(rep('steelblue', 8), 'magenta')
+  )
+
+# 
+# df$Start  <- as.Date(df$Start, format = "%m/%d/%Y")
+# client    <- "Sample Client"
+# cols      <- RColorBrewer::brewer.pal(length(unique(df$Resource)), name = "Set3")
+#factor(df$Resource, labels = cols)
+
+p <- plot_ly()
+for(i in 1:(nrow(gantt_df))){
+  p <- add_trace(p,
+                 x = c(gantt_df$start_date[i], gantt_df$end_date[i]), 
+                 y = c(i, i), 
+                 mode = "lines",
+                 line = list(color = gantt_df$color[i], width = 20),
+                 showlegend = F,
+                 hoverinfo = "text",
+                 text = paste("Event: ", gantt_df$modules[i], "<br>",
+                              "Completion: ", gantt_df$completion[i]),
+                 evaluate = T
+  )
+}
+
+p
